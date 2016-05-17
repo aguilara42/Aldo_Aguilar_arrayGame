ad6b8645167f5f24a3afe8a3cdd628aa07e55c20
D22 <aguilara42@ninja23.d22.lan>
2016-05-17 11:21:28 -0500

    Tuesday

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -5,40 +5,53 @@ import java.util.Scanner;
 
 public class Battleship {
 
+    public static Player player;
+
     static Scanner sc = new Scanner(System.in);
     public static String direction;
+    public static int traps;
     public static String[][] map = new String[30][30];
-    public static int[] player = new int[2];
+    public static int[] coordinets = new int[2];
     public static int[][] enemy = new int[2][2];
     public static int[][] chest = new int[2][2];
+    public static int[][] trap = new int[50][2];
     public static Random rand = new Random();
     public static int health = 10;
     public static int score = 0;
+    public static String line;
 
     public static void main(String[] args) {
         startUp();
     }
 
     public static void startUp() {
-        player[0] = 14;
-        player[1] = 14;
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 1; j++) {
-                int x = rand.nextInt(29);
-                enemy[i][j] = x;
+        player = new Player(14, 14);
+        traps = rand.nextInt(40 - 10);
+
+        for (int i = 0; i < traps; i++) {
+            for (int j = 0; j < 2; j++) {
+                int b = rand.nextInt(29);
+                int a = rand.nextInt(29);
+                trap[i][j] = a;
+                trap[i][j] = b;
             }
         }
-
+        intro();
+        generate();
         while (1 < 2) {
             generateMap();
             update();
-            //updateEnemy();
             drawMap();
             userInput();
             enemyMove();
             if (health <= 0) {
                 health = 0;
                 lose();
+
+            }
+
+            if (score == 2) {
+                win();
             }
         }
     }
@@ -48,17 +61,46 @@ public class Battleship {
         System.out.println("Score: " + score);
         System.out.println("What diection would you like to move?");
         direction = sc.nextLine().toLowerCase();
-        movePlayer();
+        player.movePlayer();
+
+    }
+
+    public static void generate() {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                int x = rand.nextInt(29);
+                int y = rand.nextInt(29);
+                enemy[i][j] = x;
+                chest[i][j] = y;
+            }
+        }
+    }
+
+    public static void intro() {
+        System.out.println("You Wake up alone lost in a desert");
+        line = sc.nextLine();
+        System.out.println("Surrounded by the beating sun, you see treaures in the distance, do you dare chase these dreams?");
+        line = sc.nextLine();
+        System.out.println("As you get up your fears begin to chase you");
+        line = sc.nextLine();
+        System.out.println("Can you make it to your dreams before you drown in your fears?");
+        line = sc.nextLine();
 
     }
 
     public static void update() {
 
-        map[player[0]][player[1]] = "@ ";
+        map[coordinets[0]][coordinets[1]] = "@ ";
+
+        for (int i = 0; i < traps; i++) {
+
+            map[trap[i][0]][trap[i][1]] = "# ";
+
+        }
 
         for (int i = 0; i < 2; i++) {
             map[enemy[i][0]][enemy[i][1]] = "E ";
-            map[chest[i][0]][chest[i][1]] = "| | ";
+            map[chest[i][0]][chest[i][1]] = "T ";
         }
 
     }
@@ -72,13 +114,21 @@ public class Battleship {
     }
 
     public static void drawMap() {
-        if (chest[0][0] == player[0] && chest[0][1] == player[1]) {
-            score++;
 
-        }
-        for (int i = 0; i < 2; i++) {
-            if (enemy[i][0] == player[0] && enemy[i][1] == player[1]) {
+        for (int i = 0; i < traps; i++) {
+            if (trap[i][0] == coordinets[0] && trap[i][1] == coordinets[1]) {
                 health--;
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            if (enemy[i][0] == coordinets[0] && enemy[i][1] == coordinets[1]) {
+                health--;
+            }
+
+            if (chest[i][0] == coordinets[0] && chest[0][1] == coordinets[1]) {
+                score++;
+
             }
         }
         for (int i = 0; i < 29; i++) {
@@ -90,32 +140,6 @@ public class Battleship {
                 }
             }
         }
-    }
-
-    public static void movePlayer() {
-        if (direction.contains("n") && direction.contains("e") && player[0] > 0 && player[1] < 28) {
-            player[0]--;
-            player[1]++;
-        } else if (direction.contains("s") && direction.contains("e") && player[0] < 28 && player[1] < 28) {
-            player[0]++;
-            player[1]++;
-        } else if (direction.contains("s") && direction.contains("w") && player[0] < 28 && player[1] > 0) {
-            player[0]++;
-            player[1]--;
-        } else if (direction.contains("n") && direction.contains("w") && player[0] > 0 && player[1] > 0) {
-            player[0]--;
-            player[1]--;
-        } else if (direction.contains("n") && player[0] > 0) {
-            player[0]--;
-        } else if (direction.contains("e") && player[1] < 28) {
-            player[1]++;
-        } else if (direction.contains("s") && player[0] < 28) {
-            player[0]++;
-        } else if (direction.contains("w") && player[1] > 0) {
-            player[1]--;
-        }
-        update();
-
     }
 
     public static void enemyMove() {
@@ -133,13 +157,13 @@ public class Battleship {
         } else if (direction.contains("n") && direction.contains("w") && enemy[i][0] > 0 && enemy[i][1] > 0) {
             enemy[i][0]--;
             enemy[i][1]--;
-        } else if (enemy[i][0] > player[0] && enemy[i][0] > 0) {
+        } else if (enemy[i][0] > coordinets[0] && enemy[i][0] > 0) {
             enemy[i][0]--;
-        } else if (enemy[i][1] < player[1] && enemy[i][1] < 28) {
+        } else if (enemy[i][1] < coordinets[1] && enemy[i][1] < 28) {
             enemy[i][1]++;
-        } else if (enemy[i][0] < player[0] && enemy[i][0] < 28) {
+        } else if (enemy[i][0] < coordinets[0] && enemy[i][0] < 28) {
             enemy[i][0]++;
-        } else if (enemy[i][1] > player[1] && enemy[i][1] > 0) {
+        } else if (enemy[i][1] > coordinets[1] && enemy[i][1] > 0) {
             enemy[i][1]--;
         }
         update();
@@ -147,11 +171,33 @@ public class Battleship {
     }
 
     public static void lose() {
+        score = 0;
         while (1 < 2) {
-            System.out.println("You have lost play again y/n?");
+            score = 0;
+            System.out.println("");
+            System.out.println("Lost play again y/n?");
             String lost = sc.nextLine().toLowerCase();
             if (lost.contains("y")) {
                 health = 10;
+                score = 0;
+                startUp();
+            } else {
+
+            }
+        }
+    }
+
+    public static void win() {
+        score = 0;
+        while (1 < 2) {
+
+            System.out.println("");
+            System.out.println("P"
+                    + "lay again y/n?");
+            String lost = sc.nextLine().toLowerCase();
+            if (lost.contains("y")) {
+                health = 10;
+                score = 0;
                 startUp();
             } else {
 
---
+++ src/battleship/Player.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+import static battleship.Battleship.chest;
+import static battleship.Battleship.coordinets;
+import static battleship.Battleship.direction;
+import static battleship.Battleship.enemy;
+import static battleship.Battleship.rand;
+
+
+/**
+ *
+ * @author aguilara42
+ */
+public class Player {
+    public static Battleship level1;
+
+    int x;
+    int y;
+    int HP;
+    char symbol = '@';
+    int score = 0;
+    int level = 1;
+
+    Player(int a, int b) {
+        this.x = a;
+        this.y = b;
+        this.HP = 10;
+        coordinets[0] = x;
+        coordinets[1] = y;
+        
+    }
+
+    public static void movePlayer() {
+        if (direction.contains("n") && direction.contains("e") && coordinets[0] > 0 && coordinets[1] < 28) {
+            coordinets[0]--;
+            coordinets[1]++;
+        } else if (direction.contains("s") && direction.contains("e") && coordinets[0] < 28 && coordinets[1] < 28) {
+            coordinets[0]++;
+            coordinets[1]++;
+        } else if (direction.contains("s") && direction.contains("w") && coordinets[0] < 28 && coordinets[1] > 0) {
+            coordinets[0]++;
+            coordinets[1]--;
+        } else if (direction.contains("n") && direction.contains("w") && coordinets[0] > 0 && coordinets[1] > 0) {
+            coordinets[0]--;
+            coordinets[1]--;
+        } else if (direction.contains("n") && coordinets[0] > 0) {
+            coordinets[0]--;
+        } else if (direction.contains("e") && coordinets[1] < 28) {
+            coordinets[1]++;
+        } else if (direction.contains("s") && coordinets[0] < 28) {
+            coordinets[0]++;
+        } else if (direction.contains("w") && coordinets[1] > 0) {
+            coordinets[1]--;
+        }
+        level1.update();
+
+    }
+}
